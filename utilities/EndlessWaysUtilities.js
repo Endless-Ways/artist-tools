0aea8682d2294d49d4024b9ef0e676840145207a
Damian at mba <damian@frey.NOSPAMco.nz>
2021-12-02 22:57:52 +0100

    aargh

--- utilities/EndlessWaysUtilities.js
+++ utilities/EndlessWaysUtilities.js
@@ -1,4 +1,4 @@
-// Get a certain number of numbers between 0 and 1 (>=0 and <1) directly from the current 
+// Get an array of numbers between 0 and 1 (>=0 and <1) directly from the current 
 // Endless Ways token seed. This works by slicing seed into roughly equal-sized chunks and 
 // interpreting each of them as a float between 0 and 1. 
 //
